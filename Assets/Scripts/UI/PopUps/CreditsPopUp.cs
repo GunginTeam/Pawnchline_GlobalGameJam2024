f1e32ad5347f8ef1d99dccae9e5304027a9a261d
Quicorax <qcolobrans@gmail.com>
2024-01-24 19:23:19 +0100

    Remote variables test

--- Assets/Scripts/UI/PopUps/CreditsPopUp.cs
+++ Assets/Scripts/UI/PopUps/CreditsPopUp.cs
@@ -1,4 +1,24 @@
+using Services.Runtime.RemoteVariables;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
 public sealed class CreditsPopUp : BaseView
 {
+    [SerializeField] private TMP_Text _testTest;
+
+    private IRemoteVariablesService _remoteVariablesService;
     
+    [Inject]
+    public void Construct(IRemoteVariablesService remoteVariablesService)
+    {
+        _remoteVariablesService = remoteVariablesService;
+    }
+
+    protected override void PreOpen()
+    {
+        base.PreOpen();
+
+        _testTest.text = _remoteVariablesService.GetString("CARD_TEXT_0");
+    }
 }
