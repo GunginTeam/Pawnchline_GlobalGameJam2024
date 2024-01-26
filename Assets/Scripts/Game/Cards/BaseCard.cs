9daa16d52f15553352025c4705aedf23a78a27cc
QuimColobrans <qcolobrans@gmail.com>
2024-01-26 22:29:02 +0100

    Card Drag

--- Assets/Scripts/Game/Cards/BaseCard.cs
+++ Assets/Scripts/Game/Cards/BaseCard.cs
@@ -1,14 +1,13 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class BaseCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class BaseCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
-    [SerializeField] private Image _background;
+    [SerializeField] private Transform _container;
 
     private bool _isHighlighted;
+    private bool _isDragging;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -22,7 +21,7 @@ public class BaseCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (!_isHighlighted)
+        if (!_isHighlighted || _isDragging)
         {
             return;
         }
@@ -30,11 +29,51 @@ public class BaseCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         HighLight(false);
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (_isDragging)
+        {
+            return;
+        }
+
+        _isDragging = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!_isDragging)
+        {
+            return;
+        }
+
+        _isDragging = false;
+
+        ResetPosition();
+    }
+
+    private void Update()
+    {
+        if (_isDragging)
+        {
+            _container.DOMove(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0), 0.25f);
+        }
+    }
+
     private void HighLight(bool highlight)
     {
         _isHighlighted = highlight;
-        
-        transform.DOMoveY(_isHighlighted ? 50 : 0, 0.25f);
-        transform.DORotate(Vector3.forward * (_isHighlighted ? 90 : 0), 0.25f);
+
+        _container.DOMoveY(_isHighlighted ? 50 : 0, 0.25f);
+        _container.DOScale(_isHighlighted ? 1.15f : 1, 0.25f);
+        _container.DORotate(Vector3.forward * (_isHighlighted ? 90 : 0), 0.25f);
+    }
+
+    private void ResetPosition()
+    {
+        _container.DORotate(Vector3.forward * 0, 0.1f).OnComplete(() =>
+        {
+            _container.DOScale(1, 0.25f);
+            _container.DOLocalMove(Vector3.zero, 0.25f);
+        });
     }
 }
