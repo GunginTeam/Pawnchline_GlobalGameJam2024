53ee7d743bc8c1bf1d3684f9e518f56dd3e8ceca
QuimColobrans <qcolobrans@gmail.com>
2024-01-26 21:57:56 +0100

    Card hand

--- Assets/Scripts/Game/Cards/BaseCard.cs
+++ Assets/Scripts/Game/Cards/BaseCard.cs
@@ -1,5 +1,40 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class BaseCard : MonoBehaviour
+public class BaseCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Image _background;
+
+    private bool _isHighlighted;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_isHighlighted)
+        {
+            return;
+        }
+
+        HighLight(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!_isHighlighted)
+        {
+            return;
+        }
+
+        HighLight(false);
+    }
+
+    private void HighLight(bool highlight)
+    {
+        _isHighlighted = highlight;
+        
+        transform.DOMoveY(_isHighlighted ? 50 : 0, 0.25f);
+        transform.DORotate(Vector3.forward * (_isHighlighted ? 90 : 0), 0.25f);
+    }
 }
