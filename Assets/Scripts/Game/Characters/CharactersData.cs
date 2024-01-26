cebd155758b2b04e6bb0649c488ce7bef4252396
Lorién <lorienportella@gmail.com>
2024-01-26 22:27:31 +0100

    characters have humor

---
+++ Assets/Scripts/Game/Characters/CharactersData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GunginJokes/CharactersSprites", fileName = "CharactersSprites")]
+public class CharactersData : ScriptableObject
+{
+    [SerializeField] private List<CharacterVisualData> _characterSprites;
+    [SerializeField] private List<HatSprite> _hatSprites;
+    [SerializeField] private HumorSet _humorSet;
+
+    public CharacterVisualData GetCharacterVisualData(CharacterType type)
+    {
+        return _characterSprites.Find(item => item._type == type);
+    }
+
+    public HatSprite GetHatSprite()
+    {
+        return _hatSprites[Random.Range(0, _hatSprites.Count)];
+    }
+
+    public CharacterHumor GetRandomHumor()
+    {
+        return _humorSet.GetRandomHumor();
+    }
+}
